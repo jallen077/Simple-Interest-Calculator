4a082748ba42ef2dcb28ef63be3b77cec0a0b944
Joshua Allen <joshallen06@gmail.com>
2021-04-04 20:00:10 +0000

    updating SIC

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ function compute()
     principal = document.getElementById("principal").value;
     rate = document.getElementById("rate").value;
     years = document.getElementById("years").value;
-
-    result = principal * years * rate / 100;
-
+    result = principal * rate * years / 100;
     // convert number of years into actual year in the future
-    document.getElementById("result").innerHTML = result;
-    document.getElementById("principal_").value = "principal"
-    document.getElementById("rate_").value = "rate"
-    document.getElementById("years_").value = "years"
-}
+    document.getElementById("result").innerHTML = "If you deposit:" + principal + "<br>At an interest rate of:" + rate + "<br>You will receive an amount of:" + result + "<br>In the year:" + years ;
+    
+ }
 
 function showVal(newVal){
     document.getElementById("rate").innerHTML=newVal;
