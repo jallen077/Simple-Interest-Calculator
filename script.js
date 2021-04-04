07f05c72548b7d802c3d3ecb94954b3cde72690f
Joshua Allen <joshallen06@gmail.com>
2021-04-04 19:04:41 +0000

    changes to SIC

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    result = principal * years * rate / 100;
+
+    // convert number of years into actual year in the future
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("principal_").value = "principal"
+    document.getElementById("rate_").value = "rate"
+    document.getElementById("years_").value = "years"
+}
+
+function showVal(newVal){
+    document.getElementById("rate").innerHTML=newVal;
 }
         
